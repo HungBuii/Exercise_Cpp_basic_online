3bdffa8ce6a55dd20c4cdbdedce255d738af7ee2
HungBuii <buiduyhungg2310@gmail.com>
2023-01-11 16:42:14 +0700

    Update 7.2 (1)
    Update 1st

--- Ex_7.2/Ex_7.2/Ex_7.2.cpp
+++ Ex_7.2/Ex_7.2/Ex_7.2.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 
-float program(int j) {
-    double x = 0;
-    x = j * 1.8 + 32;
-    return x; 
+using namespace std;
+
+float program(float x) {
+    float result = (float)(x * 1.8 + 32);
+    return result;
 }
 
-int main()
-{
-    
-    std::cout << "Ket qua 1: " << std::endl;
-    
-    for (int i = 0; i < 11; i++) {
-        
-        std::cout << i << " (C) = " << program(i) << " (F)" << std::endl;
+int main() {
+    std::cout << "Ket qua 1:\n";
+    for (int i = 0; i <= 10; i++) {
+        std::cout << "\t" << i << " (C) = " << program(i) << " (F)\n";
     }
-
-    std::cout << "Ket qua 2: " << std::endl;
-    int c = 0;
-    std::cout << "Enter degree C: ";
-    std::cin >> c;
-    std::cout << c << " (C) = " << program(c) << " (F)" << std::endl;
-
+    std::cout << "\nNhap do C: ";
+    float celsius = 0;
+    std::cin >> celsius;
+    std::cout << "Ket qua 2: " << program(celsius);
     return 0;
 }
-
