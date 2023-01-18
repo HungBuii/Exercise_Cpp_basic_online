7929effde9be64ef4cee5ad3cf3fa2c93532327b
HungBuii <buiduyhungg2310@gmail.com>
2023-01-18 22:38:37 +0700

    Update 8.4(1)

--- Ex_8.4/Ex_8.4/Ex_8.4.cpp
+++ Ex_8.4/Ex_8.4/Ex_8.4.cpp
@@ -1,66 +1,110 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-double program_1(int a[], int length) {
-    double average = 0;
-    int sum = 0;
-    int count = 0;
-    for (int i = 0; i < length; i++) {
-        if (i % 2 == 0) {
-            sum += a[i];
-            count++;
-        }
-    }
-    average = (double)sum / count;
-    return average;
+float program_1(int a[], int length) {
+	float result_1 = 0;
+	int index_pos = 0;
+	for (int i = 0; i < length; i++) {
+		if (i % 2 == 0) {
+			result_1 += a[i];
+			index_pos += 1;
+		}
+	}
+	return result_1 / index_pos;
 }
 
-double program_2(int a[], int length) {
-    double average_odd = 0;
-    int sum_odd = 0;
-    int count = 0;
-    for (int i = 0; i < length; i++) {
-        if (i % 2 != 0 && a[i] % 2 != 0) {
-            sum_odd += a[i];
-            count++;
-        }
-    }
-    average_odd = (double)sum_odd / count;
-    return average_odd;
+float program_2(int a[], int length) {
+	float result_2 = 0;
+	int index_pos = 0;
+	for (int i = 0; i < length; i++) {
+		if (i % 2 != 0 && a[i] % 2 != 0) {
+			result_2 += a[i];
+			index_pos += 1;
+		}
+	}
+	// for(int i = 1; i < length; i+=2)
+	// if(a[i] % 2 != 0)
+	return result_2 / index_pos;
 }
 
-double program_3(int a[], int length) {
-    int max = a[0];
-    for (int i = 0; i < length; i++) {
-        if (max < a[i]) {
-            max = a[i];
-        }
-    }
-    return max;
+int program_3(int a[], int length) {
+	int max = a[0];
+	for (int i = 0; i < length; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
 }
 
-double program_4(int a[], int length) {
-
+int program_4(int a[], int length) {
+	int min_index = 0;
+	for (int i = 0; i < length; i++) {
+		if (a[i] < a[min_index]) {
+			min_index = i;
+		}
+	}
+	return min_index;
 }
 
-int main()
-{
-    const int MAX = 1000;
-    int a[MAX];
+int program_5(int a[], int length) {
+	int nums_square = 0;
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < program_3(a, length) / 2; j++) {
+			if (a[i] == pow(j, 2)) nums_square++;
+		}
+	}
+	// for(int i = 0; i < length; i++)
+	// if(sqrt(a[i]) == (int)sqrt(a[i])) nums_square++;
+	return nums_square;
+}
 
-    cout << "Nhap so luong phan tu: ";
-    int n; cin >> n;
+bool check_prime(int x) {
+	if (x <= 1) return false;
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) return false;
+	}
+	return true;
+}
 
-    cout << "Nhap lien tiep " << n << " gia tri: ";
-    for (int i = 0; i < n; i++) cin >> a[i];
+int program_6(int a[], int length) {
+	int result_6 = 0;
+	for (int i = 0; i < length; i++) {
+		if (check_prime(a[i]) == true) result_6 += a[i];
+	}
+	return result_6;
+}
 
-    cout << "Ket qua 1: " << program_1(a, n) << "\n";
-    cout << "Ket qua 2: " << program_2(a, n) << "\n";
-    cout << "Ket qua 3: " << program_3(a, n) << "\n";
-    cout << "Ket qua 4: " << program_4(a, n) << "\n";
-    //cout << "Ket qua 5: " << program_5(a, n) << "\n";
-    //cout << "Ket qua 6: " << program_6(a, n) << "\n";
+void program_7(int a[], int length) {
+	for (int i = 0; i < length; i++) {
+		for (int j = i + 1; j < length; j++) {
+			if (a[i] > a[j]) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
 
-    //cout << "Ket qua 7: "; program_7(a, n);
-    for (int i = 0; i < n; i++) cout << a[i] << " ";
+int main() {
+	const int MAX = 1000;
+	int a[MAX];
+	cout << "Nhap so luong phan tu: ";
+	int n; cin >> n;
+
+	cout << "Nhap lien tiep " << n << " gia tri: ";
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	cout << "Ket qua 1: " << program_1(a, n) << "\n";
+	cout << "Ket qua 2: " << program_2(a, n) << "\n";
+	cout << "Ket qua 3: " << program_3(a, n) << "\n";
+	cout << "Ket qua 4: " << program_4(a, n) << "\n";
+	cout << "Ket qua 5: " << program_5(a, n) << "\n";
+	cout << "Ket qua 6: " << program_6(a, n) << "\n";
+
+	cout << "Ket qua 7: "; program_7(a, n);
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
 }
