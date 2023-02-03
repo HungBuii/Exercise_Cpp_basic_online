d29afa40269f05337fd47121d521f776c7799d13
HungBuii <buiduyhungg2310@gmail.com>
2023-02-03 10:03:06 +0700

    Create dynamic_array.cpp

---
+++ dynamic_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 3;
+    int* array = new int[size] {1, 2, 3};
+    
+    int new_size = 6;
+    int* resize = new int[new_size];
+    
+    for (int i = 0; i < size; i++) {
+        resize[i] = array[i];
+    }
+    
+    delete[] array;
+    
+    array = resize;
+    size = new_size;
+    
+    // delete[] resize; gay ra loi doi voi 3 so dau boi vi array dang tro den dia chi cua resize
+    
+    for (int i = 3; i < size; i++) {
+        array[i] = 5;
+    }
+    
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    
+    
+    // delete[] array;
+    // delete[] resize; khong delete 2 mang tren thi se in ra dc mang 6 phan tu nhung nen delete de tra lai vung nho mang cho he dieu hanh
+    return 0;
+}
