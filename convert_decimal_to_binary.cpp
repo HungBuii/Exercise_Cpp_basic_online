8aca6e243e8fbabbcd9bc9bf74dce448e8332083
HungBuii <buiduyhungg2310@gmail.com>
2023-02-03 11:10:48 +0700

    Create convert_decimal_to_binary.cpp

---
+++ convert_decimal_to_binary.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long Dec2Bin(int decimalNumber) 
+{
+    long long binaryNumber = 0;
+    int p = 0;
+    while (decimalNumber > 0)
+    {
+        binaryNumber += (decimalNumber % 2) * pow(10, p);
+        ++p;
+        decimalNumber /= 2;
+    }
+    return binaryNumber; // cac so hoat dong nhu bo nho stack 
+}
+
+int main() {
+	int decimalNumber;
+	cout << "Enter a decimal number: ";
+	cin >> decimalNumber;
+	
+	cout << "Bin = " << Dec2Bin(decimalNumber) << endl;
+	
+	return 0;
+}
