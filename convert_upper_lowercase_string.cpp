830ce3b1f5f95046e174a408b7c7b7a70ae4239f
HungBuii <buiduyhungg2310@gmail.com>
2023-02-03 10:45:27 +0700

    Update convert_upper_lowercase_string.cpp

--- convert_upper_lowercase_string.cpp
+++ convert_upper_lowercase_string.cpp
@@ -7,13 +7,21 @@ using namespace std;
 int main() {
 	char name[] = "Hung Bui"; // it is "Hung Bui\0"
 	// const array and can't fix length of array
-	for (int i = 0; i < sizeof(name) - 1; i++) { // remove \0: null character
-		if (name[i] >= 'a' && name[i] <= 'z') { // uppercase
-			name[i] -= 32;
-		}
-//		if (name[i] >= 'A' && name[i] <= 'Z') { // lowercase
-//			name[i] += 32;
+	
+	// Cach 1
+//	for (int i = 0; i < sizeof(name) - 1; i++) { // remove \0: null character
+//		if (name[i] >= 'a' && name[i] <= 'z') { // uppercase
+//			name[i] -= 32;
 //		}
+////		if (name[i] >= 'A' && name[i] <= 'Z') { // lowercase
+////			name[i] += 32;
+////		}
+//	}
+
+	// Cach 2: su dung STL
+	for (int i = 0; i < sizeof(name) - 1; i++) {
+		name[i] = toupper(name[i]); // tolower(name[i])
+		// toupper va tolower chi bien doi 1 ki tu don nen muon doi chuoi thi phai duyet for
 	}
 	std::cout << name; // trong chuoi ki tu thi khong phai duyet for boi vi 
 					   // ki tu khong the su dung cac phep toan nen no chi phuc vu de in ra. 
