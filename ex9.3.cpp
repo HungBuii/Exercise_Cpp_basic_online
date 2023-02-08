e22c3adb638d043352d1b28308d176a6cf23effe
HungBuii <buiduyhungg2310@gmail.com>
2023-02-08 16:16:43 +0700

    Update ex9.3.cpp

--- ex9.3.cpp
+++ ex9.3.cpp
@@ -12,38 +12,60 @@ int program_1(std::string fullname, char x) {
 }
 
 int program_2(std::string fullname) {
-	int num_space = fullname.find(' ');
-	int count = 1;
-	while (num_space != std::string::npos) {
-		fullname = fullname.substr(num_space + 1);
-		num_space = fullname.find(' ');
-		count++;
-	}
-	return count;
+	// cach 1
+//	int num_space = fullname.find(' ');
+//	int count = 1;
+//	while (num_space != std::string::npos) {
+//		fullname = fullname.substr(num_space + 1);
+//		num_space = fullname.find(' ');
+//		count++;
+//	}
+//	return count;
+
+	// cach 2
+	int num_space = program_1(fullname, ' ');
+	return num_space + 1;
 }
 
 bool program_3(std::string s, std::string s1) {
-	int first_space = s.find(' ');
-	std::string last_name;
-	last_name = s.substr(0, first_space);
-	if (last_name == s1) return true;
-	return false;
+	// cach 1
+//	int first_space = s.find(' ');
+//	std::string last_name;
+//	last_name = s.substr(0, first_space);
+//	if (last_name == s1) return true;
+//	return false;
+	
+	// cach 2
+	if(s.find(s1) == 0) 
+        return true;
+        
+    return false;
 }
 
 std::string program_4(std::string s) {
 	int first_space = s.find(' ');
 	std::string last_name;
-	last_name = s.substr(0, first_space);
+	if (first_space != std::string::npos) {
+		last_name = s.substr(0, first_space);	
+	}
 	return last_name;
 }
 
 std::string program_5(std::string s) {
-	int num_space = s.find(' ');
-	while(num_space != std::string::npos) {
-		s = s.substr(num_space + 1);
-		num_space = s.find(' ');
-	}
-	return s;
+	// cach 1
+//	int num_space = s.find(' ');
+//	while(num_space != std::string::npos) {
+//		s = s.substr(num_space + 1);
+//		num_space = s.find(' ');
+//	}
+//	return s;
+
+	// cach 2
+	for(int i = s.length() - 1; i >= 0; i--)
+    {
+        if(s[i] == ' ') 
+            return s.substr(i+1);
+    }
 }
 
 int main() {
